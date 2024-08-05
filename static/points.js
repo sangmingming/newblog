ad30c54eef21fd403960334d0033d1521acd1d99
sam sang <linming1007@gmail.com>
2024-08-05 19:08:25 +0800

    update theme style

--- static/points.js
+++ static/points.js
@@ -19,7 +19,7 @@ let points = [
     ["<b>京都</b><i>Kyoto</i><a href='https://isming.me/2018-04-japan/'><img src='https://isming.me/images/photo/japan/DSC06750.jpg' />日本关西蜜月旅行</a>", 34.9675192,135.7797101],
     ["<b>奈良</b><i>Nara-ken</i><a href='https://isming.me/2018-04-japan/'><img src='https://isming.me/images/photo/japan/DSC07306.jpg' />日本关西蜜月旅行</a>", 34.6838455,135.8483084],
     ["<b>宣城</b><i>Xuancheng</i><a href='https://isming.me/2024-04-xuancheng/'><img src='https://img.isming.me/photo/20240429-17-52.jpg' />看皖南山水-皖南川藏线及查济桃花潭自驾游</a>", 30.4832636,118.1494118],
-    ["<b>景德镇</b><i>Jingdezhen</i><a href='/2023-02-jingdezhen/'><img src='/images/photo/jingdezhen/20230204-05-27.jpg' />景德镇一日匆匆游</a>", 29.3582395,117.2735595],
+    ["<b>景德镇</b><i>Jingdezhen</i><a href='/2023-02-jingdezhen/'><img src='/images/photo/jingdezhen/20230204-05-27.jpg' />景德镇一日匆匆游</a>", 29.2881451,117.2166864],
     ["<b>旧金山</b><i>San Francisco</i><a href='/2018-05-us/'><img src='/images/photo/america/DSC07744.jpg' />美国湾区参加Google IO大会之行</a>", 37.8199067,-122.47858],
     ["<b>成都</b><i>Chengdu</i><a href='/2024-03-chengdu-chongqing/'><img src='https://img.isming.me/photo/20240317-16-517.jpg' />一场美食美景之旅-记成都重庆游</a>", 30.9965254,103.6142723],
     ["<b>重庆</b><i>Chongqing</i><a href='/2024-03-chengdu-chongqing/'><img src='https://img.isming.me/photo/20240320-16-92.jpg' />一场美食美景之旅-记成都重庆游</a>", 29.5650177,106.5750892],
